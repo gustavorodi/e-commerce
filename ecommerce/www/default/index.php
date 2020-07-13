d9854aa43cea1b85f18ae1d49b9c6b3b04c459eb
gustavorodi <gustavocaetano.rodrigues@gmail.com>
2020-07-13 20:42:30 -0300

    Pronto

---
+++ ecommerce/www/default/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once("header/header.php");
+?>
+<html>
+
+<head>
+    <style>
+        body {
+            background-color: #96005aaf;
+        }
+    </style>
+</head>
+<body>
+</body>
+</html>
